df235eadb6a7b8f22f0750cc9a57b8a9270b5722
Denis Denes <ddenes@cloudbasesolutions.com>
2014-06-13 18:53:51 +0300

    Fixes Hyper-V SCSI slot selection
    Currently, when attaching volumes, the Hyper-V driver selects the
    slot on the SCSI controller by using the number of drives
    attached to that controller.
    
    This leads to exceptions when detaching volumes having lower
    numbered slots and then attaching a new volume, as Hyper-V will
    then try to use an occupied slot.
    
    This patch fixes the issue by simply getting a list of used slots
    and getting a slot that it's not among those. It also checks
    for the slot number to be smaller than the maximum number of slots
    per SCSI controller.
    
    Fixes bug: #1329764
    
    (cherry-picked from commit a73e31022c9fef532cc2d66462d13f4ab2358565)
    
    Change-Id: Id0c09c4debc848004a78971670cc3caef2f77ce3
    
    Conflicts:
    	nova/tests/virt/hyperv/test_hypervapi.py

--- nova/tests/virt/hyperv/test_hypervapi.py
+++ nova/tests/virt/hyperv/test_hypervapi.py
@@ -165,7 +165,7 @@ class HyperVAPIBaseTestCase(test.NoDBTestCase):
         self._mox.StubOutWithMock(vmutils.VMUtils, 'set_nic_connection')
         self._mox.StubOutWithMock(vmutils.VMUtils, 'get_vm_scsi_controller')
         self._mox.StubOutWithMock(vmutils.VMUtils, 'get_vm_ide_controller')
-        self._mox.StubOutWithMock(vmutils.VMUtils, 'get_attached_disks_count')
+        self._mox.StubOutWithMock(vmutils.VMUtils, 'get_attached_disks')
         self._mox.StubOutWithMock(vmutils.VMUtils,
                                   'attach_volume_to_controller')
         self._mox.StubOutWithMock(vmutils.VMUtils,
@@ -1133,6 +1133,8 @@ class HyperVAPITestCase(HyperVAPIBaseTestCase):
         fake_mounted_disk = "fake_mounted_disk"
         fake_device_number = 0
         fake_controller_path = 'fake_scsi_controller_path'
+        self._mox.StubOutWithMock(self._conn._volumeops,
+                                  '_get_free_controller_slot')
 
         self._mock_login_storage_target(target_iqn, target_lun,
                                         target_portal,
@@ -1152,7 +1154,8 @@ class HyperVAPITestCase(HyperVAPIBaseTestCase):
             m.AndReturn(fake_controller_path)
 
             fake_free_slot = 1
-            m = vmutils.VMUtils.get_attached_disks_count(fake_controller_path)
+            m = self._conn._volumeops._get_free_controller_slot(
+                fake_controller_path)
             m.AndReturn(fake_free_slot)
 
         m = vmutils.VMUtils.attach_volume_to_controller(instance_name,
@@ -1747,6 +1750,20 @@ class VolumeOpsTestCase(HyperVAPIBaseTestCase):
                 self.assertRaises(exception.NotFound,
                                   self.volumeops._get_mounted_disk_from_lun,
                                   target_iqn, target_lun)
+
+    def test_get_free_controller_slot_exception(self):
+        fake_drive = mock.MagicMock()
+        type(fake_drive).AddressOnParent = mock.PropertyMock(
+            side_effect=xrange(constants.SCSI_CONTROLLER_SLOTS_NUMBER))
+        fake_scsi_controller_path = 'fake_scsi_controller_path'
+
+        with mock.patch.object(self.volumeops._vmutils,
+                'get_attached_disks') as fake_get_attached_disks:
+            fake_get_attached_disks.return_value = (
+                [fake_drive] * constants.SCSI_CONTROLLER_SLOTS_NUMBER)
+            self.assertRaises(vmutils.HyperVException,
+                              self.volumeops._get_free_controller_slot,
+                              fake_scsi_controller_path)
 
 
 class HostOpsTestCase(HyperVAPIBaseTestCase):

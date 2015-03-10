c393be311a693c640a23bd111ff1343dec1568bb
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2015-03-10 10:51:36 +0200

    Hyper-V: restart serial console workers after instance power change
    At the moment, the only way the Hyper-V handles unexpected power
    state changes is via the manager periodic task. The issue is that
    unexpected instance reboots are not handled at all.
    
    After an instance is stopped, suspended or paused, the console log
    workers are stopped as well as the serial console pipe is closed.
    Those workers are never restarted unless a power change is triggered
    by the manager.
    
    This patch proposes an instance state event listener, emiting events
    that can be handled by the manager, similar to the Libvirt driver
    workflow.
    
    Change-Id: Ia0a875707a7a37224efb333feab1503e83449cc4
    Closes-Bug: #1428505

--- nova/virt/hyperv/vmutils.py
+++ nova/virt/hyperv/vmutils.py
@@ -27,6 +27,7 @@ if sys.platform == 'win32':
 
 from oslo_config import cfg
 from oslo_log import log as logging
+from oslo_utils import uuidutils
 
 from nova import exception
 from nova.i18n import _, _LW
@@ -764,3 +765,23 @@ class VMUtils(object):
             if v.EnabledState == constants.HYPERV_VM_STATE_ENABLED]
 
         return active_vm_names
+
+    def get_vm_state_change_listener(self, event_type, timeframe):
+        return self._conn.Msvm_ComputerSystem.watch_for(
+            event_type,
+            delay_secs=timeframe,
+            fields=["EnabledState"])
+
+    def _get_instance_notes(self, vm_name):
+        vm = self._lookup_vm_check(vm_name)
+        vmsettings = self._get_vm_setting_data(vm)
+        return [note for note in vmsettings.Notes.split('\n') if note]
+
+    def get_instance_uuid(self, vm_name):
+        instance_notes = self._get_instance_notes(vm_name)
+        if instance_notes and uuidutils.is_uuid_like(instance_notes[0]):
+            return instance_notes[0]
+
+    def get_vm_power_state(self, vm_enabled_state):
+        return self._enabled_states_map.get(vm_enabled_state,
+                                            constants.HYPERV_VM_STATE_OTHER)
